1156c9f5f4113b8c40607924c4468bd0c80b6c22
Daniel Lewensztain <a2019114917@estudiantes.upsa.edu.bo>
2020-08-29 08:19:56 -0400

    Con controlador students

---
+++ EstudianteSeguro/EstudianteSeguro/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace EstudianteSeguro.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext 
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<EstudianteSeguro.Models.Student> Students { get; set; }
+    }
+}
